de8c8453752858c5bd14c15cceafd5bf19fe077f
tangyu <yu.tang@pitayagames.com>
2020-05-07 17:51:13 +0800

    原型模式

---
+++ DesignPattern/PrototypePattern.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace DesignPattern
+{
+    public abstract class PrototypePattern
+    {
+        public int ID { get; set; }
+
+        public List<int> Data { get; set; }
+
+        public abstract PrototypePattern Copy();
+    }
+
+    public class TestData01 : PrototypePattern
+    {
+        public override PrototypePattern Copy()
+        {
+            return (TestData01)this.MemberwiseClone();
+        }
+    }
+
+    //此处MemberwiseClone是浅复制
+}
