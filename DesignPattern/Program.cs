131f16837d1751d59aaa6b5f5faa2a145cbbdafc
tangyu <yu.tang@pitayagames.com>
2020-05-09 11:05:24 +0800

    原型模式深复制

--- DesignPattern/Program.cs
+++ DesignPattern/Program.cs
@@ -33,6 +33,19 @@ namespace DesignPattern
             director.Construct(lowBuilder);
             lowBuilder.GetComputer().Show();
 
+            TestData02 t2 = new TestData02();
+            t2.ID = 2;
+            t2.Data = new System.Collections.Generic.List<int>();
+            t2.Data.Add(1);
+            t2.Data.Add(2);
+
+            TestData02 t2copy = (TestData02)t2.Copy();
+            t2copy.ID = 3;
+            t2copy.Data.Clear();
+
+            Console.WriteLine($"{t2.ID}/{t2.Data.Count}");
+            Console.WriteLine($"{t2copy.ID}/{t2copy.Data.Count}");
+
             Console.WriteLine("done.");
             Console.ReadLine();
         }
--- DesignPattern/PrototypePattern.cs
+++ DesignPattern/PrototypePattern.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.IO;
 
 namespace DesignPattern
 {
@@ -20,4 +21,46 @@ namespace DesignPattern
     }
 
     //此处MemberwiseClone是浅复制
+
+    public class TestData02 : PrototypePattern
+    {
+        public override PrototypePattern Copy()
+        {
+            using (MemoryStream writeMs = new MemoryStream())
+            {
+                BinaryWriter binaryWriter = new BinaryWriter(writeMs);
+
+                binaryWriter.Write(ID);
+                binaryWriter.Write(Data.Count);
+
+                for (int i = 0; i < Data.Count; i++)
+                {
+                    binaryWriter.Write(Data[i]);
+                }
+
+                binaryWriter.Close();
+                byte[] copyData = writeMs.ToArray();
+
+                using (MemoryStream readerMs = new MemoryStream(writeMs.ToArray()))
+                {
+                    BinaryReader binaryReader = new BinaryReader(readerMs);
+                    TestData02 copyResult = new TestData02();
+                    copyResult.ID = binaryReader.ReadInt32();
+                    copyResult.Data = new List<int>(binaryReader.ReadInt32());
+
+                    for (int i = 0; i < copyResult.Data.Count; i++)
+                    {
+                        var data = binaryReader.ReadInt32();
+                        copyResult.Data.Add(data);
+                    }
+
+                    binaryReader.Close();
+
+                    return copyResult;
+                }
+            }
+        }
+    }
+
+    //可使用序列化方式进行深复制
 }
